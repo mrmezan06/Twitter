98b09988a76060897fd0e4b6f74c2a039216b3d5
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-30 18:51:02 +0600

    Follower Card Almost done

---
+++ FrontEnd/src/Data/FollowersData.js
@@ -0,0 +1,27 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  {
+    name: "Andrew Thomas",
+    username: "andrewthomas",
+    img: img1,
+  },
+  {
+    name: "Hulk Buster",
+    username: "hulkbuster",
+    img: img2,
+  },
+  {
+    name: "Thor",
+    username: "ThunderMaster",
+    img: img3,
+  },
+  {
+    name: "Natasha Romanoff",
+    username: "NatashaRomanoff",
+    img: img4,
+  },
+];
--- FrontEnd/src/components/profileSide/ProfileSide.jsx
+++ FrontEnd/src/components/profileSide/ProfileSide.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./ProfileSide.css";
 import LogoSearch from "../LogoSearch/LogoSearch";
 import ProfileCard from "../ProfileCard/ProfileCard";
+import FollowersCard from "../FollowersCard/FollowersCard";
 
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
       <LogoSearch />
       <ProfileCard />
+      <FollowersCard />
     </div>
   );
 };
 
 export default ProfileSide;
+
+// 41:40
