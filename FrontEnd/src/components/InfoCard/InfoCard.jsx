23d3a520070b824e7111da279e21dc737c1bdc82
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-31 03:45:11 +0600

    ProfileModal Basic Setup Done
    https://mantine.dev/

--- FrontEnd/src/components/InfoCard/InfoCard.jsx
+++ FrontEnd/src/components/InfoCard/InfoCard.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
+import ProfileModal from "../ProfileModal/ProfileModal";
 
 const InfoCard = () => {
+  const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="InfoCard">
       <div className="infoHead">
         <h4>Your Info</h4>
         <div>
-          <UilPen width="2rem" height="1.2rem" />
+          <UilPen
+            width="2rem"
+            height="1.2rem"
+            onClick={() => setModalOpened(true)}
+          />
+          <ProfileModal
+            modalOpened={modalOpened}
+            setModalOpened={setModalOpened}
+          />
         </div>
       </div>
       <div className="info">
---
+++ FrontEnd/src/components/ProfileModal/ProfileModal.jsx
@@ -0,0 +1,25 @@
+import { Modal, useMantineTheme } from "@mantine/core";
+
+function ProfileModal({ modalOpened, setModalOpened }) {
+  const theme = useMantineTheme();
+
+  return (
+    <Modal
+      overlayColor={
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[9]
+          : theme.colors.gray[2]
+      }
+      overlayOpacity={0.55}
+      overlayBlur={3}
+      opened={modalOpened}
+      onClose={() => setModalOpened(false)}
+    >
+      <form className="infoForm">
+        <h3>Your info</h3>
+      </form>
+    </Modal>
+  );
+}
+
+export default ProfileModal;
