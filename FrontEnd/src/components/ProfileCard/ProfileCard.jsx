dccede38718dbca5b432edbf4547b307f78fb343
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-30 18:14:18 +0600

    Profile Card Ready

---
+++ FrontEnd/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./ProfileCard.css";
+import Cover from "../../img/cover.jpg";
+import Profile from "../../img/profileImg.jpg";
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+      <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+      </div>
+      <div className="ProfileName">
+        <span>Zendaya MJ</span>
+        <span>Senior UI/UX Designer</span>
+      </div>
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            <span>6,890</span>
+            <span>Followers</span>
+          </div>
+          <div className="vl"></div>
+          <div className="follow">
+            <span>1</span>
+            <span>Followings</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <span>My Profile</span>
+    </div>
+  );
+};
+
+export default ProfileCard;
