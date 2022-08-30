35c7cbe5f7ed008b88adefacfe3758eeb099be58
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-30 17:16:29 +0600

    font install and blur positioning

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,8 +1,9 @@
-import "./App.css"
+import "./App.css";
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
     </div>
   );
 }
