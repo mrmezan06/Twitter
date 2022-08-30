9fc2200bf754cbaa61468d6473e473e63c6855c9
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-31 03:58:22 +0600

    Profile Modal Completed

--- FrontEnd/src/components/ProfileModal/ProfileModal.jsx
+++ FrontEnd/src/components/ProfileModal/ProfileModal.jsx
@@ -1,4 +1,5 @@
 import { Modal, useMantineTheme } from "@mantine/core";
+import "./ProfileModal.css";
 
 function ProfileModal({ modalOpened, setModalOpened }) {
   const theme = useMantineTheme();
@@ -12,11 +13,63 @@ function ProfileModal({ modalOpened, setModalOpened }) {
       }
       overlayOpacity={0.55}
       overlayBlur={3}
+      size="55%"
       opened={modalOpened}
       onClose={() => setModalOpened(false)}
     >
       <form className="infoForm">
         <h3>Your info</h3>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="FirstName"
+            placeholder="First name"
+          />
+          <input
+            type="text"
+            className="infoInput"
+            name="LastName"
+            placeholder="Last name"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="worksAT"
+            placeholder="Works at"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="livesIN"
+            placeholder="Lives in"
+          />
+          <input
+            type="text"
+            className="infoInput"
+            name="Country"
+            placeholder="Country"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="relationshipStatus"
+            placeholder="Relationship status"
+          />
+        </div>
+        <div>
+          Profile Image
+          <input type="file" name="profileImg" />
+          Cover Image
+          <input type="file" name="coverImg" />
+        </div>
+        <button className="button info-button">Update</button>
       </form>
     </Modal>
   );
