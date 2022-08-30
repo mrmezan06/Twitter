970c75927bfa6b61688f2bf048a656038ba1aab2
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-30 17:22:00 +0600

    Grid Layout to divide full page in 3 section

---
+++ FrontEnd/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="profileSide">Profile</div>
+      <div className="postSide">Post</div>
+      <div className="rightSide">Right</div>
+    </div>
+  );
+};
+
+export default Home;
