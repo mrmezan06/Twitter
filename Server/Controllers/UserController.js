1e138de6f8d5c7c5a84607b5186bf345eb033b09
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-01 22:37:10 +0600

    Update user

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -1,4 +1,5 @@
 import userModel from "../Models/userModel.js";
+import bcrypt from "bcrypt";
 
 // get a user
 export const getUser = async (req, res) => {
@@ -6,7 +7,8 @@ export const getUser = async (req, res) => {
   try {
     const user = await userModel.findById({ _id: id });
     if (user) {
-      return res.status(200).json(user);
+      const { password, ...rest } = user._doc;
+      return res.status(200).json(rest);
     } else {
       return res.status(404).send("User not found!");
     }
@@ -15,4 +17,28 @@ export const getUser = async (req, res) => {
   }
 };
 
-// 51:31
+// update a user
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus, password } = req.body;
+  try {
+    if (id === currentUserId || currentUserAdminStatus) {
+      if (password) {
+        const salt = bcrypt.genSaltSync(10);
+        req.body.password = bcrypt.hashSync(password, salt);
+      }
+      const user = await userModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(user);
+    } else {
+      return res
+        .status(401)
+        .json({
+          message: "Unauthorized! You can only update your own account!",
+        });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser } from "../Controllers/UserController.js";
+import { getUser, updateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
 router.get("/:id", getUser);
+router.put("/:id", updateUser);
 
 export default router;
