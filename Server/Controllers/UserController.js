f0a647ebbc6370fefa1cd313701ffb987096381a
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-10 14:40:40 +0600

    Delete User and Follow Controller Completed

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -41,4 +41,46 @@ export const updateUser = async (req, res) => {
   }
 };
 
-// 1:07:59
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus } = req.body;
+  try {
+    if (id === currentUserId || currentUserAdminStatus) {
+      await userModel.findByIdAndDelete(id);
+      return res.status(200).json({ message: "User has been deleted!" });
+    } else {
+      return res.status(401).json({
+        message: "Unauthorized! You can only delete your own account!",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+// Follow a user
+export const followUser = async (req, res) => {
+  // get the user id from the url
+  const id = req.params.id;
+  // get the user id from the request body which is the current user
+  const { currentUserId } = req.body;
+  try {
+    if (id !== currentUserId) {
+      const user = await userModel.findById(id);
+      const currentUser = await userModel.findById(currentUserId);
+      if (!user.followers.includes(currentUserId)) {
+        // add current user to user's followers
+        await user.updateOne({ $push: { followers: currentUserId } });
+        // add user to current user's following
+        await currentUser.updateOne({ $push: { following: id } });
+        res.status(200).json("User has been followed!");
+      } else {
+        res.status(403).json("You already follow this user!");
+      }
+    } else {
+      res.status(403).json("You can't follow yourself!");
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getUser, updateUser } from "../Controllers/UserController.js";
+import {
+  getUser,
+  updateUser,
+  deleteUser,
+  followUser,
+} from "../Controllers/UserController.js";
 
 const router = express.Router();
 
 router.get("/:id", getUser);
 router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.put("/:id/follow", followUser);
 
 export default router;
