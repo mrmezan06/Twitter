ec0672f9de7384cc9282eaf8c0b4e0faf3950a1b
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-18 01:37:50 +0600

    Reducers added with Combine Reducer

---
+++ FrontEnd/src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+const authReducer = (
+  state = { authData: null, loading: false, error: false },
+  action
+) => {
+  switch (action.type) {
+    case "AUTH_START":
+      return { ...state, loading: true, error: false };
+    case "AUTH_SUCCESS":
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      return { ...state, loading: false, authData: action?.data, error: false };
+    case "AUTH_FAIL":
+      return { ...state, loading: false, error: true };
+    // case "LOGOUT":
+    // localStorage.clear();
+    // return { ...state, authData: null };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
---
+++ FrontEnd/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import authReducer from "./authReducer";
+
+export const reducers = combineReducers({
+  auth: authReducer,
+});
