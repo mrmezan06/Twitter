6aed6ef3bd4f7e5c6063a61958eb85aa3e5d2c63
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-22 22:47:04 +0600

    Fetching post okay needs assign picture url

--- FrontEnd/src/components/Posts/Posts.jsx
+++ FrontEnd/src/components/Posts/Posts.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Posts.css";
-import { PostsData } from "../../Data/PostsData";
+// import { PostsData } from "../../Data/PostsData";
 import Post from "../Post/Post";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Posts = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth.authData);
+  const [posts, setPosts] = React.useState([]);
+
+  const getTimelinePosts = async (id) => {
+    const data = await axios.get(`http://localhost:5000/post/${id}/timeline`);
+    setPosts(data.data);
+  };
+
+  useEffect(() => {
+    getTimelinePosts(user?._id);
+  }, [dispatch, user?._id]);
+
   return (
     <div className="Posts">
-      {PostsData.map((post, id) => {
+      {posts.map((post, id) => {
         return <Post data={post} id={id} />;
       })}
     </div>
